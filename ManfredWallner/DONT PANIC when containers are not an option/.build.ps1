0f692533d8c15040b6f36cd27ae3e9e42fe4e5f8
Manfred Wallner <schusterfredl@mwallner.net>
2022-06-22 10:07:21 +0200

    sessions by mwallner

---
+++ ManfredWallner/DONT PANIC when containers are not an option/.build.ps1
@@ -0,0 +1,14 @@
+
+Get-ChildItem (Join-Path $PSScriptRoot "../ib.tasks.*.ps1") | ForEach-Object {
+    . $_.FullName
+}
+
+# Synposis: use 'marp' to generate .pdf and .html files from the markdown presentation
+Task Build GetMarp, {
+    $filename = "dont_panic"
+    & $script:_the_marp "${filename}.md" -o "${filename}.pdf" --allow-local-files --html
+    & $script:_the_marp "${filename}.md" -o "${filename}.html" --html
+}
+
+# Synposis: the default task: build
+Task . Build
