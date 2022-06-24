b585109eefb781ec0c6a6bda9ca47b3e384eebdf
Ben Reader <ben.reader@gmail.com>
2022-06-24 17:05:48 +0200

    reorganizing everything

--- PowerShell_Intune_Measure_Once/demo1.ps1
+++
@@ -1,5 +0,0 @@
-. wt
-
-#region CHEAT SHEET
-Build-IntuneConfigReport -Tenant Intune.training -OutputFolder "C:\data\reports\"
-#endregion
--- PowerShell_Intune_Measure_Once/obviousVirus.ps1
+++
@@ -1,2 +0,0 @@
-write-host "hey siri, play 'Say no more mon amour!' by Rex Manning"
-start https://www.youtube.com/watch?v=szvt8iWJ0oo
